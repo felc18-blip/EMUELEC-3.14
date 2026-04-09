3a250b576c266d47ca75792ae75b7418bfd2e655
felc18-blip <fel.c18@gmail.com>
2026-04-09 16:36:45 -0300

    sx05re: drastic-advanced: fix starter to use local libs

--- packages/sx05re/emulators/drastic-advanced/advancedrastic.sh
+++ packages/sx05re/emulators/drastic-advanced/advancedrastic.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# 1. Carrega as variáveis de ambiente do sistema
 . /etc/profile
 
-@LIBEGL@
-
+# 2. Define onde o emulador e as libs estão
 CONFIG_DIR="/storage/.config/drastic-advanced"
 
+# 3. Prioriza as suas libs customizadas sobre as do sistema
+export LD_LIBRARY_PATH="${CONFIG_DIR}/libs:$LD_LIBRARY_PATH"
+
+# 4. Entra na pasta para garantir que os arquivos de configuração sejam lidos
 cd "${CONFIG_DIR}"
 
+# 5. Lança o emulador passando o jogo e substitui o processo do shell
 exec ./drastic "$@"
