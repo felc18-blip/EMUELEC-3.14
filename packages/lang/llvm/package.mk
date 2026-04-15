90dca73849a667d3663ba784b0b31c20cda538a2
felc18-blip <fel.c18@gmail.com>
2026-04-15 02:35:28 -0300

    devel: update toolchain (llvm, ccache) and compression libs

--- packages/lang/llvm/package.mk
+++ packages/lang/llvm/package.mk
@@ -3,10 +3,11 @@
 # Copyright (C) 2018-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="llvm"
-PKG_VERSION="17.0.6"
+PKG_VERSION="21.1.8"
+PKG_SHA256="4633a23617fa31a3ea51242586ea7fb1da7140e426bd62fc164261fe036aa142"
 PKG_LICENSE="Apache-2.0"
 PKG_SITE="http://llvm.org/"
-PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION}.src.tar.xz"
+PKG_URL="https://github.com/llvm/llvm-project/releases/download/llvmorg-${PKG_VERSION}/llvm-project-${PKG_VERSION/-/}.src.tar.xz"
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain llvm:host zlib"
 PKG_LONGDESC="Low-Level Virtual Machine (LLVM) is a compiler infrastructure."
@@ -19,7 +20,6 @@ PKG_CMAKE_OPTS_COMMON="-DLLVM_INCLUDE_TOOLS=ON \
                        -DLLVM_INCLUDE_EXAMPLES=OFF \
                        -DLLVM_BUILD_TESTS=OFF \
                        -DLLVM_INCLUDE_TESTS=OFF \
-                       -DLLVM_INCLUDE_GO_TESTS=OFF \
                        -DLLVM_BUILD_BENCHMARKS=OFF \
                        -DLLVM_INCLUDE_BENCHMARKS=OFF \
                        -DLLVM_BUILD_DOCS=OFF \
@@ -28,7 +28,6 @@ PKG_CMAKE_OPTS_COMMON="-DLLVM_INCLUDE_TOOLS=ON \
                        -DLLVM_ENABLE_SPHINX=OFF \
                        -DLLVM_ENABLE_OCAMLDOC=OFF \
                        -DLLVM_ENABLE_BINDINGS=OFF \
-                       -DLLVM_ENABLE_TERMINFO=OFF \
                        -DLLVM_ENABLE_ASSERTIONS=OFF \
                        -DLLVM_ENABLE_WERROR=OFF \
                        -DLLVM_ENABLE_ZLIB=OFF \
@@ -43,20 +42,49 @@ PKG_CMAKE_OPTS_COMMON="-DLLVM_INCLUDE_TOOLS=ON \
                        -DLLVM_ENABLE_Z3_SOLVER=OFF \
                        -DCMAKE_SKIP_RPATH=ON"
 
+if listcontains "${GRAPHIC_DRIVERS}" "(iris|panfrost)"; then
+  PKG_DEPENDS_UNPACK="spirv-headers spirv-llvm-translator"
+  PKG_CMAKE_OPTS_COMMON+=" -DLLVM_SPIRV_INCLUDE_TESTS=OFF"
+fi
+
+post_unpack() {
+  if listcontains "${GRAPHIC_DRIVERS}" "(iris|panfrost)"; then
+    mkdir -p "${PKG_BUILD}"/llvm/projects/{SPIRV-Headers,SPIRV-LLVM-Translator}
+      tar --strip-components=1 \
+        -xf "${SOURCES}/spirv-headers/spirv-headers-$(get_pkg_version spirv-headers).tar.gz" \
+        -C "${PKG_BUILD}/llvm/projects/SPIRV-Headers"
+      tar --strip-components=1 \
+        -xf "${SOURCES}/spirv-llvm-translator/spirv-llvm-translator-$(get_pkg_version spirv-llvm-translator).tar.gz" \
+        -C "${PKG_BUILD}/llvm/projects/SPIRV-LLVM-Translator"
+  fi
+}
+
 pre_configure() {
   PKG_CMAKE_SCRIPT=${PKG_BUILD}/llvm/CMakeLists.txt
 }
 
 pre_configure_host() {
-  case "${TARGET_ARCH}" in
-    "arm")
-      LLVM_BUILD_TARGETS="X86\;ARM"
-      ;;
+  case "${MACHINE_HARDWARE_NAME}" in
     "aarch64")
-      LLVM_BUILD_TARGETS="X86\;AArch64"
+      LLVM_BUILD_TARGETS="AArch64"
+      ;;
+    "arm")
+      LLVM_BUILD_TARGETS="ARM"
       ;;
     "x86_64")
-      LLVM_BUILD_TARGETS="X86\;AMDGPU"
+      LLVM_BUILD_TARGETS="X86"
+      ;;
+  esac
+
+  case "${TARGET_ARCH}" in
+    "aarch64")
+      LLVM_BUILD_TARGETS+="\;AArch64"
+      ;;
+    "arm")
+      LLVM_BUILD_TARGETS+="\;ARM"
+      ;;
+    "x86_64")
+      LLVM_BUILD_TARGETS+="\;X86\;AMDGPU"
       ;;
   esac
 
@@ -71,13 +99,26 @@ pre_configure_host() {
 }
 
 post_make_host() {
-  ninja ${NINJA_OPTS} llvm-config llvm-tblgen
+  ninja ${NINJA_OPTS} llc llvm-ar llvm-as llvm-config llvm-cov llvm-dis \
+                      llvm-link llvm-nm llvm-objcopy llvm-objdump \
+                      llvm-profdata llvm-readobj llvm-size llvm-strip \
+                      llvm-tblgen opt
+
+  if listcontains "${GRAPHIC_DRIVERS}" "(iris|panfrost)"; then
+    ninja ${NINJA_OPTS} llvm-spirv
+  fi
 }
 
 post_makeinstall_host() {
   mkdir -p ${TOOLCHAIN}/bin
-    cp -a bin/llvm-config ${TOOLCHAIN}/bin
-    cp -a bin/llvm-tblgen ${TOOLCHAIN}/bin
+    cp -a bin/{llc,llvm-ar,llvm-as,llvm-config,llvm-cov,llvm-dis} "${TOOLCHAIN}/bin"
+    cp -a bin/{llvm-link,llvm-nm,llvm-objcopy,llvm-objdump} "${TOOLCHAIN}/bin"
+    cp -a bin/{llvm-profdata,llvm-readobj,llvm-size,llvm-strip} "${TOOLCHAIN}/bin"
+    cp -a bin/{llvm-tblgen,opt} "${TOOLCHAIN}/bin"
+
+  if listcontains "${GRAPHIC_DRIVERS}" "(iris|panfrost)"; then
+    cp -a bin/llvm-spirv "${TOOLCHAIN}/bin"
+  fi
 }
 
 pre_configure_target() {
