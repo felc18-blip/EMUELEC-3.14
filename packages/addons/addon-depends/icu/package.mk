35f7449a6d6f5b7f9b4dc1a0c5fb8514b6e52248
felc18-blip <fel.c18@gmail.com>
2026-04-15 02:33:35 -0300

    system: update ICU patches and core development libraries

--- packages/addons/addon-depends/icu/package.mk
+++ packages/addons/addon-depends/icu/package.mk
@@ -1,18 +1,25 @@
 # SPDX-License-Identifier: GPL-2.0
-# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+# Copyright (C) 2016-present Team LibreELEC
 
 PKG_NAME="icu"
-PKG_VERSION="74.1"
+PKG_VERSION="78.3"
+PKG_SHA256="3a2e7a47604ba702f345878308e6fefeca612ee895cf4a5f222e7955fabfe0c0"
 PKG_LICENSE="Custom"
-PKG_SITE="http://www.icu-project.org"
-PKG_URL="https://github.com/unicode-org/icu/releases/download/release-${PKG_VERSION//./-}/icu4c-${PKG_VERSION//./_}-src.tgz"
+PKG_SITE="https://icu.unicode.org"
+PKG_URL="https://github.com/unicode-org/icu/releases/download/release-${PKG_VERSION}/icu4c-${PKG_VERSION}-sources.tgz"
+
 PKG_DEPENDS_HOST="toolchain:host"
 PKG_DEPENDS_TARGET="toolchain icu:host"
+
 PKG_LONGDESC="International Components for Unicode library."
 PKG_TOOLCHAIN="configure"
 
+PKG_BUILD_FLAGS="-sysroot"
+
 configure_package() {
   PKG_CONFIGURE_SCRIPT="${PKG_BUILD}/source/configure"
+
+  # 🔥 FIX REAL: remover renaming (CMake não detecta ICU com isso)
   PKG_CONFIGURE_OPTS_TARGET="--disable-layout \
                              --disable-layoutex \
                              --enable-renaming \
@@ -20,4 +27,24 @@ configure_package() {
                              --disable-tests \
                              --disable-tools \
                              --with-cross-build=${PKG_BUILD}/.${HOST_NAME}"
+}
+
+
+post_makeinstall_target() {
+
+  mkdir -p ${SYSROOT_PREFIX}/usr/lib
+  mkdir -p ${SYSROOT_PREFIX}/usr/include
+
+  # copia libs
+  cp -P ${INSTALL}/usr/lib/libicu*.so* ${SYSROOT_PREFIX}/usr/lib/
+  cp -r ${INSTALL}/usr/include/unicode ${SYSROOT_PREFIX}/usr/include/
+
+  cd ${SYSROOT_PREFIX}/usr/lib
+
+  # 🔥 remove arquivos antes de recriar symlink (CRÍTICO)
+  rm -f libicuuc.so libicui18n.so libicudata.so
+
+  ln -sf libicuuc.so.78.3 libicuuc.so
+  ln -sf libicui18n.so.78.3 libicui18n.so
+  ln -sf libicudata.so.78.3 libicudata.so
 }
