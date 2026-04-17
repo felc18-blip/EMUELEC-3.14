8767da96a620c118d63a0c0a13150b39c6871bfb
felc18-blip <fel.c18@gmail.com>
2026-04-16 21:11:49 -0300

    toolchain: update rust and targets

--- packages/rust/rustc-snapshot/package.mk
+++ packages/rust/rustc-snapshot/package.mk
@@ -10,15 +10,15 @@ PKG_TOOLCHAIN="manual"
 
 case "${MACHINE_HARDWARE_NAME}" in
   "aarch64")
-    PKG_SHA256="628f57a45eb8143a7ac1acd5d6d01e3ae3cdf1ad11d151795ed765f6e5f3047e"
+    PKG_SHA256="fb4ff31b71d7a2b6a793fb88fac18429befe807ddab3472bf87cbf92b6e5fbf6"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
   "arm")
-    PKG_SHA256="117a67a06e74de1ad386b340a50332ffd5cce3aac58d07b972091c94eee10d51"
+    PKG_SHA256="9228f0a8a00a75fe43f3c80fc3f8d73431aa75535bdd59a2dfdf704a7b9f6031"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnueabihf.tar.xz"
     ;;
   "x86_64")
-    PKG_SHA256="14f383eb4d6e65ce01cc99f2c5cf5a78744239f29704f72fe84f11095af779f5"
+    PKG_SHA256="0a6b16ca476461c6238f48efcb74a7aa08a3983e61adf587f208ba48504d87e1"
     PKG_URL="https://static.rust-lang.org/dist/rustc-${PKG_VERSION}-${MACHINE_HARDWARE_NAME}-unknown-linux-gnu.tar.xz"
     ;;
 esac
