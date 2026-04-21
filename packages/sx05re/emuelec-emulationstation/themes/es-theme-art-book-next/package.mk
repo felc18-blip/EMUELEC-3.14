ffacd995d1b12b2e5d5daf5846a92a9fde36b24f
felc18-blip <fel.c18@gmail.com>
2026-04-21 16:19:26 -0300

    EmulationStation: update es_features, es_systems and art-book-next theme
    Also remove deprecated es-theme-EmuELEC-carbon.

--- packages/sx05re/emuelec-emulationstation/themes/es-theme-art-book-next/package.mk
+++ packages/sx05re/emuelec-emulationstation/themes/es-theme-art-book-next/package.mk
@@ -2,15 +2,12 @@
 # Copyright (C) 2020 Shanti Gilbert (https://github.com/shantigilbert)
 # Copyright (C) 2020 351ELEC team (https://github.com/fewtarius/351ELEC)
 # Copyright (C) 2023 JELOS (https://github.com/JustEnoughLinuxOS)
-
+#
 PKG_NAME="es-theme-art-book-next"
-PKG_VERSION="f18dc79707e70c0435bf64a6e2cb88803ff0ac03"
-PKG_ARCH="any"
+PKG_VERSION="9a50ef366e750aabfab29e6915a2867607212971"
 PKG_LICENSE="CUSTOM"
-PKG_SITE="https://github.com/felc18-blip/art-book-next"
-PKG_URL="${PKG_SITE}.git"
-GET_HANDLER_SUPPORT="git"
-PKG_SHORTDESC="Art Book Next"
+PKG_SITE="https://github.com/anthonycaccese/art-book-next-es"
+PKG_URL="https://github.com/anthonycaccese/art-book-next-es/archive/${PKG_VERSION}.tar.gz"
 PKG_LONGDESC="Art Book Next"
 PKG_TOOLCHAIN="manual"
 
